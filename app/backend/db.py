c18edd26585b2f18943774be24213555eca5f62b
Dmitrii <nudumka22@gmail.com>
2025-02-25 15:29:25 +0500

    update project

--- app/backend/db.py
+++ app/backend/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from app.backend.db_config import settings
 
+
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
